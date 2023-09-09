ac6659f371bc11428891b0fa9fb1ab8126c397be
Tarcizo Neto <tarcizon12@gmail.com>
2023-09-09 15:45:28 -0300

    Dividindo topicos do docs em cada funcionalidade do código

--- tarefa-de-regressao/main.py
+++ tarefa-de-regressao/main.py
@@ -1,15 +1,20 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-# Dados
+#1. visualização dos dados com um gráfico de dispressão 
 Data = np.loadtxt("base-de-dados/Ice_cream selling data.csv",delimiter=',', skiprows = 1)
 temperatura = Data[:, 0]  # Armazena a primeira coluna em coluna1
 vendas_sorvete = Data[:, 1]
 
-print(temperatura)
+
+#2. variaveis regressoras sejam armazenadas em uma matriz de dimensão N×p
+##  O mesmo para o vetor de varaiveis observadas, organizando em um vetor de dimensão N×1
+
+X = np.vstack((temperatura)).T
+y = np.array(vendas_sorvete).reshape(-1, 1)
 
 
- ##Criar o gráfico de dispersão
+#1. visualização dos dados com um gráfico de dispressão
 plt.scatter(temperatura, vendas_sorvete, label='Dados de Vendas de Sorvete')
 plt.xlabel('Temperatura (°C)')
 plt.ylabel('Vendas de Sorvete (unidades)')
@@ -17,8 +22,8 @@ plt.title('Relação entre Temperatura e Vendas de Sorvete')
 plt.legend()
 plt.grid(True)
 
-X = np.vstack((temperatura)).T
-y = np.array(vendas_sorvete).reshape(-1, 1)
+print("Variavel de regressão: ", X , '\n')
+print("Varaivel observada: ", y , '\n')
 
-## Mostrar o gráfico
+#1. visualização dos dados com um gráfico de dispressão
 plt.show()
