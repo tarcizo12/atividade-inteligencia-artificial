cda7be7e7c9bdd2bea2b5563522894d24ef0bee1
Tarcizo Neto <tarcizon12@gmail.com>
2023-09-17 20:00:53 -0300

    Ajustes na hora de armazenar os dados com suas devidas dimensões

--- tarefa-de-regressao/main.py
+++ tarefa-de-regressao/main.py
@@ -20,18 +20,28 @@ TITULOS_EIXOS = {
 #3.Assim, defina essa quantidade de rodadas com o valor 1000.
 RODADAS_DE_TREINAMENTO = 1000
 
-#1.Extração de dados para visualização dos dados com um gráfico de dispressão 
+#1.Extração de dados para visualização dos dados com um gráfico de dispressão
 Data = np.loadtxt("base-de-dados/Ice_cream selling data.csv",delimiter=',', skiprows = 1)
 temperatura = Data[:, 0]  # Armazena a primeira coluna em coluna1
 vendas_sorvete = Data[:, 1]
+
+N = temperatura.shape[0]
+p = 1
+X = temperatura.copy().reshape(N,p)
+Y = vendas_sorvete.copy().reshape(N, 1)
+
+##y = np.array(vendas_sorvete).reshape(-1, 1)
+
+print(X, "temperatura")
+print(Y, "vendas")
 
 #1. Plotando grafico
 criarGrafico(
     TITULOS_EIXOS["X"], TITULOS_EIXOS["Y"],
     'Dados de Vendas de Sorvete','Relação entre Temperatura e Vendas de Sorvete',
-    temperatura, vendas_sorvete, True)
+    X, Y, False)
 
 
 #2. variaveis regressoras sejam armazenadas em uma matriz de dimensão N×p e o mesmo para varaiveis observadas, organizando em um vetor de dimensão N×1
-X = np.vstack((temperatura)).T
-y = np.array(vendas_sorvete).reshape(-1, 1)
+#ESTAMOS INDO NO CAMINHO CERTO
+#TEMOS QUE ADICIONAR O INTERCEPTOR NO VETOR X (POR QUE FAZER ISSO)
