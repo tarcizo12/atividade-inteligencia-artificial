d3ac9744866510b6aff81926bd3045e387c86083
Tarcizo Neto <tarcizon12@gmail.com>
2023-09-17 22:24:08 -0300

    Informações para modelos de MQO tradicional e Media de valores observaveis

--- tarefa-de-regressao/main.py
+++ tarefa-de-regressao/main.py
@@ -18,7 +18,7 @@ TITULOS_EIXOS = {
 }
 
 #3.Assim, defina essa quantidade de rodadas com o valor 1000.
-RODADAS_DE_TREINAMENTO = 1
+RODADAS_DE_TREINAMENTO = 1000 
 
 #1.Extração de dados para visualização dos dados com um gráfico de dispressão
 Data = np.loadtxt("base-de-dados/Ice_cream selling data.csv",delimiter=',', skiprows = 1)
@@ -34,7 +34,7 @@ Y = vendas_sorvete.copy().reshape(N, 1)
 #4.
 MQO_TRADICIONAL = [] #Modelo com intercepitor
 
-MQO_REGULARIZADO = [] #Modelo sem interceptor (MQO regularizado)
+MQO_REGULARIZADO = [] #to do
 
 MSE_MEDIA = [] #Modelo: Média de valores observáveis
 
@@ -42,19 +42,68 @@ MSE_MEDIA = [] #Modelo: Média de valores observáveis
 for r in range(RODADAS_DE_TREINAMENTO):
     indexRandom = np.random.permutation(N)
     indexOfOitentaPorCento = int(N*.8)
-    #Embaralhar dados
+    zero = np.zeros((1,1))
+
+    #6. Embaralhar dados
     X_embaralhado = X[indexRandom,:]
     Y_embaralhado = Y[indexRandom,:]
 
-    #Amostra para treino e teste 
+    #6. Amostra para treino e teste 
     X_treino = X_embaralhado[0: indexOfOitentaPorCento,:] #Ir de Zero até o index 80% total (no caso é 39)
     Y_treino = Y_embaralhado[0: indexOfOitentaPorCento,:]
     X_teste =  X_embaralhado[indexOfOitentaPorCento: N,:] #Ir do ultimo index que representa os 80% até o fim
     Y_teste =  Y_embaralhado[indexOfOitentaPorCento: N,:]
 
+    #Media dos valores de Y
+    mediaDosValoresDeTeste_Y = np.mean(Y_treino)
+    mediaDosValoresDeTeste_Y = np.array([
+        [mediaDosValoresDeTeste_Y],
+        [0],
+    ])
+
+    #Modelo com interceptor (MQO)
+    sizeX_treino = X_treino.shape[0]
+    interceptorTreino = np.ones((sizeX_treino , 1)) 
+    X_treino = np.concatenate((interceptorTreino , X_treino),axis=1)
+    MODELO_MQO_TRADICIONAL = np.linalg.pinv(X_treino.T@X_treino)@X_treino.T@Y_treino 
     
-    print(X_teste.shape)
-    print(Y_teste.shape)
+
+    #Calcular EQM
+    interceptorTeste = np.ones((X_teste.shape[0],1))
+    X_teste = np.concatenate((interceptorTeste,X_teste),axis=1)
+
+    predicaoMedia = X_teste@mediaDosValoresDeTeste_Y
+    predicaoModeloTradicional = X_teste@MODELO_MQO_TRADICIONAL
+
+    #print(predicaoMedia, "predicao media")
+    #print(perdicaoModeloTradicional, "predicao tradicional")
+    
+    #Determinnar distancias
+    MSE_MEDIA.append(np.mean((Y_teste-predicaoMedia)**2))
+    MQO_TRADICIONAL.append(np.mean((Y_teste-predicaoModeloTradicional)**2))
+
+#Médias de EQM
+media_MVO = np.mean(MSE_MEDIA)
+media_MQO_TRADICIONAL = np.mean(MQO_TRADICIONAL)
+
+#DESVIO PADRÃO DE EQM
+desvioPadrao_MVO = np.std(MSE_MEDIA)
+desvioPadrao_MQO_TRADICIONAL = np.std(MQO_TRADICIONAL)
+
+#Maiores e menores valores
+menorValor_MVO = np.amin(MSE_MEDIA)
+maiorValor_MVO = np.amax(MSE_MEDIA)
+menorValor_MQO_TRADICIONAL = np.amin(MQO_TRADICIONAL)
+maiorValor_MQO_TRADICIONAL = np.amax(MQO_TRADICIONAL)
+
+
+
+print(media_MVO, "Media para o MVO \n")
+print(media_MQO_TRADICIONAL, "Media para o MQO TRADICIONAL \n")
+print(desvioPadrao_MVO , "Desvio Padrao para o MVO \n")
+print(desvioPadrao_MQO_TRADICIONAL, "Desvio Padrao para o MQO TRADICIONAL \n")
+print(menorValor_MVO, maiorValor_MVO, "Menor e maior valor para MVO \n")
+print(menorValor_MQO_TRADICIONAL, maiorValor_MQO_TRADICIONAL, "Menor e maior valor para MQO tradicional \n")
 
 #1. Plotando grafico
 criarGrafico(
